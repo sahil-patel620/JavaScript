b4d14fbdb8eba0d77e84d59c3da2fd41cddf1b11
sahil-patel620 <Sp453771@gmail.com>
2025-02-10 22:28:05 +0530

    array part 1

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -15,11 +15,10 @@ myArr.pop()   // remove last element
 myArr.unshift(9)   // push at startinng
 myArr.shift()      // pop from starting
 
-console.log(myArr.includes(9));
-console.log(myArr.indexOf(3));
+console.log(myArr.includes(9));  // to check element is present in aaray or not
+console.log(myArr.indexOf(3));   // to get index of any element from array
 
-const newArr = myArr.join()
-
+const newArr = myArr.join()     // converts array's elements into string 
 console.log(myArr);
 console.log( newArr);
 
@@ -27,13 +26,12 @@ console.log( newArr);
 // slice, splice
 
 console.log("A ", myArr);
-
+// slice return a array where ending index element is excluded and do not effect original array
 const myn1 = myArr.slice(1, 3)
-
 console.log(myn1);
 console.log("B ", myArr);
 
-
+// splice return a array where ending index is also included and remove these index elements from original array
 const myn2 = myArr.splice(1, 3)
-console.log("C ", myArr);
 console.log(myn2);
+console.log("C ", myArr);
