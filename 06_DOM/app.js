9c58f3dd7ca60684e44e25b533eabca3ed7eb928
sahil-patel620 <Sp453771@gmail.com>
2025-02-16 16:57:12 +0530

    update

---
+++ 06_DOM/app.js
@@ -0,0 +1,35 @@
+ // function to add list
+ function addLanguage(langName){
+    const li = document.createElement('li');
+    li.innerHTML = `${langName}`
+    document.querySelector('.language').appendChild(li)
+}
+addLanguage("Python")
+addLanguage("Typescript")
+addLanguage("Java")
+addLanguage('C++')
+
+//function to add list using createTextNode
+function addOptiLanguage(langName){
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(langName))
+    document.querySelector('.language').appendChild(li)
+}
+addOptiLanguage('golang')
+
+//Edit 1st way
+const secondLang = document.querySelector("li:nth-child(2)")
+console.log(secondLang);
+secondLang.innerHTML = "Mojo"
+//replace 
+const newli = document.createElement('li')
+newli.textContent = "Mojo"
+secondLang.replaceWith(newli)
+
+//edit 2nd way 
+const firstLang = document.querySelector("li:first-child")
+firstLang.outerHTML = '<li>TypeScript</li>'
+
+//remove
+const lastLang = document.querySelector('li:last-child')
+lastLang.remove()
